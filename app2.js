cf802b2d89778a7f3e4a4692808c596d8bf9fa70
uedayusuke <uedayusuke1107@gmail.com>
2020-05-20 18:23:56 +0900

    ReSturdy

---
+++ app2.js
@@ -0,0 +1,29 @@
+var hello = "Hello World!";
+alert(hello);
+
+var int1 = 1;
+var int2 = -10
+var float1 = 3.14
+var str1 = "JavaScriptを覚えよう"
+
+alert(int1);
+alert(int2);
+alert(float1);
+alert(str1);
+
+alert("四則演算");
+alert(4 + 3);
+alert(8 - 5);
+alert(2 * 6);
+alert(10 / 2)
+
+alert("文字列の結合");
+alert("Hello" + "Wolrd");
+
+alert("変数を用いて文字列の結合");
+var str2 = "Hello";
+var str3 = "Wolrd";
+var str4 = "!!";
+alert(str2 + str3);
+alert("!!を変数で追加");
+alert(str2 + str3 + str4);
